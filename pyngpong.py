41abe5ba40813a9a98cea449c2eb778b6104a609
Saul Montes De Oca <mr.sauce97@gmail.com>
2020-03-11 13:11:26 -0600

    Adding the score

--- pyngpong.py
+++ pyngpong.py
@@ -61,6 +61,20 @@ def paddleB_down():
     y -= 20
     paddle_b.sety(y)
 
+#Score
+score_a = 0
+score_b = 0
+
+
+#Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
 #Keyboard binding
 wn.listen()
 wn.onkeypress(paddleA_up, "w")
@@ -86,9 +100,17 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
+
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24, "normal"))
+
     #Collisions
     if ball.xcor() > 335 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(335)
